095501b0e0e6ce96c3e63a4f7a6209bcd19c4b50
jgoyret <j.goyret1475@gmail.com>
2024-01-04 18:57:26 -0300

    scroll control version 1

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -4,9 +4,11 @@ import {
     Environment,
     Center,
     Sparkles,
-    useGLTF
+    useGLTF,
+    ScrollControls
  } from '@react-three/drei'
  import { PerspectiveCamera } from '@theatre/r3f'
+ import CameraControl from './CameraControl'
 
 
 
@@ -15,33 +17,27 @@ export default function Experience()
 
     return(
         <>
-            
-            <PerspectiveCamera 
-                theatreKey="Camera" 
-                makeDefault 
-                position={[0, 0, -5]} 
-                fov={75} 
-                // lookAt={[0,0,0]}
-            />
+            <ScrollControls pages={3}  damping={0.1} maxSpeed={0.1}>
 
-            <Environment
-                background
-                preset='city'
-                blur={0.9}
-            />
+                <CameraControl/>
+                <Environment
+                    background
+                    preset='city'
+                    blur={0.9}
+                />
 
-            <Sparkles
-                count = {1000}
-                /** Size of particles (default: randomized between 0 and 1) */
-                size = {15}
-                /** The space the particles occupy (default: 1) */
-                scale = {100}
-                /** Movement factor (default: 1) */
-                noise = {0.5}
-            />
-            <CriaturasPeleando url={'peleaDeCriaturas.glb'}/>
+                <Sparkles
+                    count = {1000}
+                    /** Size of particles (default: randomized between 0 and 1) */
+                    size = {15}
+                    /** The space the particles occupy (default: 1) */
+                    scale = {100}
+                    /** Movement factor (default: 1) */
+                    noise = {0.5}
+                />
+                <CriaturasPeleando url={'peleaDeCriaturas.glb'}/>
 
-
+            </ScrollControls>
         </>
     )
 }

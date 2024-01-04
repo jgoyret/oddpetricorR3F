bae848bfe10d0481bde538210f5eb481299f8177
jgoyret <j.goyret1475@gmail.com>
2024-01-04 00:11:44 -0300

    first theater sheet and placeholder model of home

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -3,7 +3,8 @@ import {
     OrbitControls,
     Environment,
     Center,
-    Sparkles
+    Sparkles,
+    useGLTF
  } from '@react-three/drei'
  import { PerspectiveCamera } from '@theatre/r3f'
 
@@ -20,26 +21,34 @@ export default function Experience()
                 makeDefault 
                 position={[0, 0, -5]} 
                 fov={75} 
+                // lookAt={[0,0,0]}
             />
 
             <Environment
                 background
-                preset='night'
+                preset='city'
                 blur={0.9}
             />
 
-            <Center>
-                <Sparkles
-                    count = {1000}
-                    /** Size of particles (default: randomized between 0 and 1) */
-                    size = {15}
-                    /** The space the particles occupy (default: 1) */
-                    scale = {100}
-                    /** Movement factor (default: 1) */
-                    noise = {0.5}
-                />
-            </Center>
+            <Sparkles
+                count = {1000}
+                /** Size of particles (default: randomized between 0 and 1) */
+                size = {15}
+                /** The space the particles occupy (default: 1) */
+                scale = {100}
+                /** Movement factor (default: 1) */
+                noise = {0.5}
+            />
+            <CriaturasPeleando url={'peleaDeCriaturas.glb'}/>
+
 
         </>
     )
+}
+
+function CriaturasPeleando({ url }) 
+{
+    const { scene } = useGLTF(url)
+    console.log(scene.scale)
+    return <primitive object={scene} scale={5}/>
 }

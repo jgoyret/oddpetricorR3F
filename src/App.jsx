89afca631e48be536a594fe010cc068e11649906
Juan Goyret <j.goyret1475@gmail.com>
2024-11-20 20:10:14 -0300

    before change to new sketch

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,40 @@
-import { Suspense } from "react";
+import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import Experience from "./Experience";
 import Loading from "./Loading";
 import ComingSoon from "./ComingSoon";
-import { getProject } from '@theatre/core'
-import studio from '@theatre/studio'
-import extension from '@theatre/r3f/dist/extension'
-import { SheetProvider } from '@theatre/r3f'
+import { getProject } from "@theatre/core";
+import studio from "@theatre/studio";
+import extension from "@theatre/r3f/dist/extension";
+import { SheetProvider } from "@theatre/r3f";
 
+const oddSheet = getProject("Odd Project").sheet("Odd Sheet");
 
-
-
-const oddSheet = getProject('Odd Project').sheet('Odd Sheet')
-
-studio.initialize()
-studio.ui.restore()
-studio.extend(extension)
-
+studio.initialize();
+studio.ui.restore();
+studio.extend(extension);
 
 function App() {
+  React.useEffect(() => {
+    oddSheet.project.ready.then(() =>
+      oddSheet.sequence.play({ iterationCount: Infinity, range: [0, 1] })
+    );
+  }, []);
 
   return (
     <>
-        <Suspense fallback={<Loading />}>
-          <Canvas
-            camera={{
-              position: [2, 0, -8],
-            }}
-          >
-            <SheetProvider sheet={oddSheet}>
-              <Experience />
-              {/* <ComingSoon/> */}
-            </SheetProvider>
-          </Canvas>
-        </Suspense>
+      <Suspense fallback={<Loading />}>
+        <Canvas
+          camera={{
+            position: [2, 0, -8],
+          }}
+        >
+          <SheetProvider sheet={oddSheet}>
+            <Experience />
+            {/* <ComingSoon/> */}
+          </SheetProvider>
+        </Canvas>
+      </Suspense>
     </>
   );
 }
--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,65 +1,65 @@
+import {
+  Environment,
+  Sparkles,
+  useGLTF,
+  ScrollControls,
+  Scroll,
+} from "@react-three/drei";
+import CameraControl from "./CameraControl";
+import Welcome from "./welcome/Welcome";
 
-import { 
-    Environment,
-    Sparkles,
-    useGLTF,
-    ScrollControls,
-    Scroll
-} from '@react-three/drei'
-import CameraControl from './CameraControl'
-import Welcome from './welcome/Welcome'
+export default function Experience() {
+  return (
+    <>
+      <ScrollControls pages={3} damping={0.1} maxSpeed={0.1}>
+        <Scroll html style={{ width: "100%", height: "100%" }}>
+          <Welcome />
+        </Scroll>
 
-export default function Experience()
-{
-
-    return(
-        <>
-            <ScrollControls pages={3}  damping={0.1} maxSpeed={0.1}>
-
-                <Scroll html style={{ width: '100%', height: '100%' }}>
-                    <Welcome/>
-                </Scroll>
-
-                <CameraControl/>
-                <Environment
-                    background
-                    preset='city'
-                    blur={0.9}
-                />
-                <Sparkles
-                    count = {1000}
-                    /** Size of particles (default: randomized between 0 and 1) */
-                    size = {15}
-                    /** The space the particles occupy (default: 1) */
-                    scale = {100}
-                    /** Movement factor (default: 1) */
-                    noise = {0.5}
-                />
-                <CriaturasPeleando url={'peleaDeCriaturas.glb'} scale={5}/>
-                <Octopus url={'octopus.glb'} scale={5} position={[-1,0,5]} rotation={[0,1,0]}/>
-
-            </ScrollControls>
-        </>
-    )
+        <CameraControl />
+        <Environment background preset="city" blur={0.9} />
+        <Sparkles
+          count={1000}
+          /** Size of particles (default: randomized between 0 and 1) */
+          size={15}
+          /** The space the particles occupy (default: 1) */
+          scale={100}
+          /** Movement factor (default: 1) */
+          noise={0.5}
+        />
+        <CriaturasPeleando url={"peleaDeCriaturas.glb"} scale={5} />
+        <Octopus
+          url={"octopus.glb"}
+          scale={5}
+          position={[-1, 0, 5]}
+          rotation={[0, 1, 0]}
+        />
+      </ScrollControls>
+    </>
+  );
 }
 
-function CriaturasPeleando({ url, scale }) 
-{
-    const { scene } = useGLTF(url)
-    return <primitive object={scene} scale={scale}/>
+function CriaturasPeleando({ url, scale }) {
+  const { scene } = useGLTF(url);
+  return <primitive object={scene} scale={scale} />;
 }
 
-function Octopus({ url, position, scale, rotation }) 
-{
-    const { scene } = useGLTF(url)
-    return <primitive object={scene} scale={scale} position={position} rotation={rotation}/>
+function Octopus({ url, position, scale, rotation }) {
+  const { scene } = useGLTF(url);
+  return (
+    <primitive
+      object={scene}
+      scale={scale}
+      position={position}
+      rotation={rotation}
+    />
+  );
 }
-
-
 
 // sheetOne.project.ready.then(() =>
-//       sheetOne.sequence.play({ iterationCount: 1, range: [45, 55] }).then(() => {
-//         sheetOne.project.ready.then(() =>
-//           sheetOne.sequence.play({ iterationCount: Infinity, range: [55, 65] }))
-//       }),
+//   sheetOne.sequence.play({ iterationCount: 1, range: [45, 55] }).then(() => {
+//     sheetOne.project.ready.then(() =>
+//       sheetOne.sequence.play({ iterationCount: Infinity, range: [55, 65] })
 //     );
+//   })
+// );
